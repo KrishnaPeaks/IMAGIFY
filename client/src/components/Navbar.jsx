88b9d2b836614b0edfa200500aa241a8646c8511
KrishnaPeaks <krishna.a3567@gmail.com>
2026-01-24 19:07:11 +0530

    .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
-function Navbar() {
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+     
+    const [user,setUser] = useState(null);
+
   return (
     <div> 
-        <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/>
+        <Link to='/'><img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/></Link>
+       <div>
+            {user ?
+            <div></div>
+            :
+            <div></div> 
+            }
+        </div> 
     </div>
   )
 }
