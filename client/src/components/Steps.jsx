0aa3a73522a81bfd04f86b20442b16fff3429070
KrishnaPeaks <krishna.a3567@gmail.com>
2026-01-25 22:52:13 +0530

    .

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -3,13 +3,13 @@ import { stepsData } from '../assets/assets'
 
 const Steps = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center my-32 '>
       <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How It Works</h1>
       <p className='text-lg text-gray-600 mb-8'>Transform Words Into Stunning Images</p>
       
-      <div>
+      <div className=' space-y-4 w-full max-w-3xl text-sm'>
         {stepsData.map((item, index) => (
-          <div key={index} className="flex items-start gap-4 mb-6">
+          <div key={index} className="flex items-start gap-4 mb-6 rounded-lg shadow-md border border-gray-600 cursor-pointer hover:scale-[1.02] transition-all duration-300">
             <img src={item.icon} alt={item.title} className="w-12 h-12" />
             <div>
               <h2 className="text-xl font-semibold">{item.title}</h2>
