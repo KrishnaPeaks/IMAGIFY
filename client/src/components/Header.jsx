c5e4bddb77db9d890a920a0331fe2c550b5decc9
KrishnaPeaks <krishna.a3567@gmail.com>
2026-02-06 17:22:31 +0530

    .

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { motion } from 'framer-motion' 
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 function Header() {
+
+    const {user,setShowLogin} = useContext(AppContext)
+    const navigate = useNavigate();
+    const onClickHandler = ()=>{
+        if(user){
+            navigate('/result')
+        }
+        else{
+            setShowLogin(true)
+        }
+    }
+
   return (
     <motion.div className='flex flex-col justify-center items-center text-center my-20'
         initial={{opacity:0.2,y:100}}
@@ -15,7 +29,7 @@ function Header() {
         </div>
         <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center ' >Turn Text to <span className='text-purple-600'>Image</span>, in seconds </h1>
         <p className='text-center max-w-xl mx-auto mt-5 '> Unleash your creativity with AI . Turn your imagination into visual art in seconds - just type, and watch the magic happen. </p>
-        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full '>Generate Images <img className='h-6 ' src={assets.star_group} alt=""/></button>
+        <button onClick={onClickHandler}   className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full '>Generate Images <img className='h-6 ' src={assets.star_group} alt=""/></button>
 
         <div className='flex flex-wrap justify-center mt-16 gap-3 '>
             {Array(6).fill('').map((item,index)=>(
