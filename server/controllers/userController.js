eb0263df0236871c30847573c7642e19db13e932
KrishnaPeaks <krishna.a3567@gmail.com>
2026-02-08 14:38:36 +0530

    getting started with middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -50,4 +50,17 @@ const loginUser = async (req, res) => {
     }
 }
 
+const userCredits = async (req, res) => {
+    try {
+        const { userId } = req.body
+
+        const user = await userModel.findById(userId)
+        res.json({ success: true, credits: user.creditBalance, user: user.name });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+
+}
+
 export { registerUser, loginUser };
