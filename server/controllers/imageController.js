37658e1cb1296e5473ed66f4f411477d119cb59a
KrishnaPeaks <krishna.a3567@gmail.com>
2026-02-09 20:04:10 +0530

    imageRoutes file created

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,5 +1,6 @@
+import axios from "axios"
 import userModel from "../models/userModel"
-
+import FormData from 'form-data'
 export const generateImage = async (req, res) => {
     try {
         const { userId, prompt } = req.body
@@ -11,6 +12,20 @@ export const generateImage = async (req, res) => {
         if (user.creditBalance === 0 || userModel.creditBalance < 0) {
             res.json({ succes: false, message: "No credit Balance", creditBalance: user.creditBalance })
         }
+
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+        const { data } = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {
+            headers: {
+                'x-api-key': process.env.CLIPDROP_API,
+            }, responseType: 'arraybuffer'
+        })
+
+        const base64Image = Buffer.from(data, 'binary').toString('base64')
+        const resultImage = `data:image/png:base64,${base64Image}`
+
+        await userModel.findByIdAndUpdate(user._id, { creditBalance: userCreditBalance - 1 })
+        res.json({ success: true, message: "Image Generated", creditBalance: user.crediBalance - 1, resultImage })
 
     } catch (error) {
         console.log(error.message)
