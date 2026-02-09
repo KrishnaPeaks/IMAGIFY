f34f26ff720b05de13b9d5b6c1948f4f05e428c3
KrishnaPeaks <krishna.a3567@gmail.com>
2026-02-09 20:54:42 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,15 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import { AppContext } from './context/AppContext'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
 
     const {showLogin} = useContext(AppContext)
 
   return (
     <div className='px-4 sm:px-10 md:px-20 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50'>
-      <Navbar/>
+      <ToastContainer position='bottom-right'/><Navbar/>
       {showLogin && <Login/> }
       <Routes>
         <Route path='/' element={<Home/>}/>
